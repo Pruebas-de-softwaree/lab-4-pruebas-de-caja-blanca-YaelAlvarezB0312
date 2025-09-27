4db500e92d56da297e3036d95a8a29f2e234506a
YaelAlvarezB0312 <yaric.alvflo@gmail.com>
2025-09-27 01:00:58 +0000

    'Sentencias'

--- process_grades.py
+++ process_grades.py
@@ -36,10 +36,10 @@ def process_grades(students):
 
 if __name__ == "__main__":
     students = [
-        {'name': 'Ana', 'grades': [80, 90, 85]},
-        {'name': 'Luis', 'grades': [70, 70, 70]},
-        {'name': 'Jorge', 'grades': []},
-        {'name': 'Marta', 'grades': [40, 45, 50]}
+        #{'name': 'Ana', 'grades': [80, 90, 85]},
+        #{'name': 'Luis', 'grades': [70, 70, 70]},
+        {'name': 'Jorge', 'grades': [51, 51, 51]},
+        {'name': 'Marta', 'grades': None}
     ]
 
     result = process_grades(students)
